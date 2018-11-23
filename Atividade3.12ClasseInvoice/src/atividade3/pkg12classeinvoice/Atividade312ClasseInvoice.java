babded7a4c773f78b0fb28567513910da880627f
Jefferson Medeiros da Silva <30291378+morpheolinkin@users.noreply.github.com>
2018-11-23 17:23:52 -0200

    Update Atividade312ClasseInvoice.java

--- Atividade3.12ClasseInvoice/src/atividade3/pkg12classeinvoice/Atividade312ClasseInvoice.java
+++ Atividade3.12ClasseInvoice/src/atividade3/pkg12classeinvoice/Atividade312ClasseInvoice.java
@@ -7,9 +7,12 @@ public class Atividade312ClasseInvoice {
         fatura [0] = new Invoice("01", "Gasolina", 5, 35.70);
         
         fatura[1] = new Invoice ("02", "Mercado", 2, 350.70);
+        fatura[2] = new Invoice ("03", "rua", 2, 450.70);
         
         System.out.println("Valor da fatura 1 é de: " + fatura[0].getInvoiceAmount());
         System.out.println("Valor da fatura 2 é de: " + fatura[1].getInvoiceAmount());
+        System.out.println("Valor da fatura 3 é de: " + fatura[2].getInvoiceAmount());
+        
         
         
     }
